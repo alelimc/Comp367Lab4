bbc65cc88662fb437f73e948f26657e5e2fbfdcb
Aleli Macapagal <amacapag@my.centennialcollege.ca>
2025-03-27 15:19:23 -0400

    Nuget commit

---
+++ AzureArtifactsProj/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UtilityLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        string testString = "Hello World";
+        Console.WriteLine($"'{testString}' starts with uppercase: {testString.StartsWithUpper()}");
+
+        string testString2 = "hello world";
+        Console.WriteLine($"'{testString2}' starts with uppercase: {testString2.StartsWithUpper()}");
+    }
+}
--- UtilityLibraries/obj/Debug/net8.0/UtilityLibraries.AssemblyInfo.cs
+++ UtilityLibraries/obj/Debug/net8.0/UtilityLibraries.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UtilityLibraries")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ef360f67100f25925aa7b3931917e03814e519")]
 [assembly: System.Reflection.AssemblyProductAttribute("UtilityLibraries")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UtilityLibraries")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
